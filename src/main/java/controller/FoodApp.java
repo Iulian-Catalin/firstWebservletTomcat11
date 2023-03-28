00e645bba63df62e1229a7464c508dadf1725ab9
icondor <icondor@sdl.com>
2023-03-28 21:38:55 +0300

    vers cu db

---
+++ src/main/java/controller/FoodApp.java
@@ -0,0 +1,73 @@
+package controller;
+
+import db.FoodDB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet("/foodapp") // sub ce denumire vreau sa fie cunoscut servletul meu de catre browser
+public class FoodApp extends HttpServlet {
+
+  //  List<String> listOfFood = new ArrayList<>();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("ce faci ma servere 11 ");
+
+        String action = req.getParameter("action");
+        if(action!=null  && action.equalsIgnoreCase("C")) {
+            addFood(req,resp);
+        }
+        else if(action!=null  && action.equalsIgnoreCase("R")) {
+            listFood(req, resp);
+        }
+
+
+
+    }
+
+
+    private void addFood(HttpServletRequest req,HttpServletResponse resp) {
+
+        String foodName = req.getParameter("foodname");
+//        listOfFood.add(foodName);
+//       // listFood(req,resp);
+
+        FoodDB db = new FoodDB();
+        db.insert(foodName);
+
+    }
+
+    private void listFood(HttpServletRequest req,HttpServletResponse resp) {
+        try {
+
+
+            FoodDB db = new FoodDB();
+            List<String> myListOFFood = db.listAllFood();
+
+          JSONObject json = new JSONObject();
+          json.put("listOfFood", myListOFFood);
+          String result = json.toString();
+          resp.setContentType("application/json");
+            PrintWriter pw = resp.getWriter();
+            pw.write(result);
+            pw.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/main/java/db/FoodDB.java
@@ -0,0 +1,68 @@
+package db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodDB {
+
+    public void  insert(String foodName) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // conectare la db cu incarcare driver
+        final String URLDB = "jdbc:postgresql://localhost:5432/grupajava";
+        final String USERNAMEDB ="postgres";
+        final String PWDDB ="postgres";
+        int val = 0; // 1
+        try {
+            Connection conn = DriverManager.getConnection(URLDB, USERNAMEDB, PWDDB);
+
+            // rulare sql
+            PreparedStatement pSt = conn.prepareStatement("insert into fn (foodname) values (?)");
+            pSt.setString(1, foodName);
+
+            val = pSt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> listAllFood() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // conectare la db cu incarcare driver
+        final String URLDB = "jdbc:postgresql://localhost:5432/grupajava";
+        final String USERNAMEDB ="postgres";
+        final String PWDDB ="postgres";
+        List<String> l = new ArrayList<>();
+        int val = 0; // 1
+        try {
+            Connection conn = DriverManager.getConnection(URLDB, USERNAMEDB, PWDDB);
+
+            // rulare sql
+            PreparedStatement pSt = conn.prepareStatement("select * from fn");
+
+            ResultSet rs = pSt.executeQuery();
+
+
+            while (rs.next()) {
+             l.add(rs.getString("foodname"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return l;
+    }
+}
